a6675f94b8bc63a0d3ec73b14a9e69ed509f1e3d
Shivam <shivam@nilenso.com>
2022-02-04 17:12:26 +0530

    Add solution to Part 2 Day 3 Problem

--- src/day_3.clj
+++ src/day_3.clj
@@ -3,13 +3,13 @@
 
 (defn- parse-input []
   (mapv #(string/split % #"")
-       (string/split (slurp "src/inputs/day-3.txt") #"\n")))
+       (string/split-lines (slurp "src/inputs/day-3.txt"))))
 
 (defn- transpose [m]
   (apply mapv vector m))
 
-(defn- element-with-max-frequency [frequencies-map]
-  (if (> (get frequencies-map "1") (get frequencies-map "0"))
+(defn- max-frequency-bit [frequencies-map]
+  (if (>= (get frequencies-map "1") (get frequencies-map "0"))
     "1"
     "0"))
 
@@ -20,9 +20,41 @@
   (let [binary-gamma-rate (->> parsed-input
                                transpose
                                (map frequencies)
-                               (map element-with-max-frequency)
+                               (map max-frequency-bit)
                                (apply str))
         binary-epsilon-rate (flip binary-gamma-rate)]
     
     (* (Long/parseLong binary-gamma-rate 2)
        (Long/parseLong binary-epsilon-rate 2))))
+
+(defn- keep-numbers-with [input current-bit-position bit-position-value]
+  (filter #(= bit-position-value
+              (nth % current-bit-position))
+          input))
+
+(defn- least-frequency-bit [frequencies-map]
+  (if (<= (get frequencies-map "0") (get frequencies-map "1"))
+    "0"
+    "1"))
+
+;; O(n^2) where n is the count of the readings from the file
+(defn life-support-rating
+  ([compare-fn input] (life-support-rating compare-fn 0 input))
+  ([compare-fn current-bit-position input]
+   (if (= 1 (count input))
+     (-> input first string/join)
+     (->> input
+          (map #(nth % current-bit-position))
+          frequencies
+          compare-fn
+          (keep-numbers-with input current-bit-position)
+          (life-support-rating compare-fn (inc current-bit-position) )))))
+
+(defn part2 [parsed-input]
+  (let [oxygen-reading (life-support-rating max-frequency-bit parsed-input)
+        co2-reading (life-support-rating least-frequency-bit parsed-input)]
+
+    (* (Long/parseLong oxygen-reading 2)
+       (Long/parseLong co2-reading 2))))
+
+;; (part2 (parse-input))
