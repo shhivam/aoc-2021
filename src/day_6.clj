1d6ef78fcc0ec92b8f1433cb5359aafd55a88f05
Shivam <shivam@nilenso.com>
2022-02-23 13:01:25 +0530

    Use `get` default value argument

--- src/day_6.clj
+++ src/day_6.clj
@@ -1,27 +1,26 @@
 (ns day-6
   (:require [clojure.string :as string]))
 
-
 (defn- parse-input []
   (frequencies (mapv #(Long/parseLong %)
-       (string/split (slurp "src/inputs/day-6.txt") #","))))
+                     (string/split (slurp "src/inputs/day-6.txt") #","))))
 
 (defn fishes-after-a-day [fishes-day]
   (assoc fishes-day
-         0 (get fishes-day 1)
-         1 (get fishes-day 2)
-         2 (get fishes-day 3)
-         3 (get fishes-day 4)
-         4 (get fishes-day 5)
-         5 (get fishes-day 6)
-         6 (+ (or (get fishes-day 7) 0)
-              (or (get fishes-day 0) 0))
-         7 (get fishes-day 8)
-         8 (get fishes-day 0)))
+         0 (get fishes-day 1 0)
+         1 (get fishes-day 2 0)
+         2 (get fishes-day 3 0)
+         3 (get fishes-day 4 0)
+         4 (get fishes-day 5 0)
+         5 (get fishes-day 6 0)
+         6 (+ (get fishes-day 7 0)
+              (get fishes-day 0 0))
+         7 (get fishes-day 8 0)
+         8 (get fishes-day 0 0)))
 
 (defn solve [fishes-days days-passed total-days]
   (if (= days-passed total-days)
-    (reduce + (filter some? (vals fishes-days)))
+    (reduce + (vals fishes-days))
     (solve (fishes-after-a-day fishes-days)
            (inc days-passed)
            total-days)))
